cdcb2b7140ea2cd48f1df824f17809acad7cd7f2
Viktor Buchkivskyi <vbuchkivskyi@opaq.com>
2021-05-12 16:34:27 +0300

    generate auth secret key or get from env var

--- ch02/chat/main.go
+++ ch02/chat/main.go
@@ -4,11 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+	"time"
 
 	"main/trace"
 
@@ -41,21 +43,39 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, data)
 }
 
+const alnum = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 var host = flag.String("host", "localhost:8080", "The host of the application.")
+
+// randSeq generates random sequence of alnum characters of fixed length
+func randSeq(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alnum[rand.Intn(len(alnum))]
+	}
+	return string(b)
+}
 
 func main() {
 
 	flag.Parse() // parse the flags
 	githubCallbackURL := "http://" + *host + "/auth/callback/github"
-	githubClientID := os.Getenv("GITHUB_CLIENT_ID")
-	githubClientSecret := os.Getenv("GITHUB_CLIENT_SECRET")
+	githubClientID := os.Getenv("GITHUB_CLIENT_ID")         // registered client ID
+	githubClientSecret := os.Getenv("GITHUB_CLIENT_SECRET") // registered client secret
+	gomniauthSecurityKey := os.Getenv("OAUTH_SECURITY_KEY") // random 64-bit secret key
+
+	if gomniauthSecurityKey == "" {
+		rand.Seed(time.Now().UnixNano())
+		gomniauthSecurityKey = randSeq(64)
+	}
 
 	fmt.Println("githubCallbackURL:", githubCallbackURL)
 	fmt.Println("githubClientID:", githubClientID)
 	fmt.Println("githubClientSecret:", githubClientSecret)
+	fmt.Println("gomniauthSecurityKey:", gomniauthSecurityKey)
 
 	// setup gomniauth
-	gomniauth.SetSecurityKey("98dfbg7iu2nb4uywevihjw4tuiyub34noilk")
+	gomniauth.SetSecurityKey(gomniauthSecurityKey)
 	gomniauth.WithProviders(
 		github.New(githubClientID, githubClientSecret, githubCallbackURL),
 	)
